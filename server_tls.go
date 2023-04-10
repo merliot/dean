ec7a60fdce5afb19adaaa16fa999c21364badb2a
Scott Feldman <sfeldma@gmail.com>
2023-04-09 21:30:10 -0700

    move server TLS func over to non-tinygo

---
+++ server_tls.go
@@ -0,0 +1,29 @@
+//go:build !tinygo
+
+package dean
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func (s *Server) ServeTLS(host string) error {
+	certsDir := "certs" + s.thinger.Id()
+
+	autocertManager := &autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(host),
+		Cache:      autocert.DirCache(certsDir),
+	}
+
+	s.TLSConfig = &tls.Config{
+		GetCertificate: autocertManager.GetCertificate,
+	}
+
+	go http.ListenAndServe(":http", autocertManager.HTTPHandler(nil))
+
+	s.Addr = ":https"
+	return(s.ListenAndServeTLS("", ""))
+}
