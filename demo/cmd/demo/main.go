24b01a921eb4ceb04e017b7b4f10888e45e302da
Scott Feldman <sfeldma@gmail.com>
2023-04-12 04:03:19 +0000

    demo: add wio

--- demo/cmd/demo/main.go
+++ demo/cmd/demo/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/merliot/dean/demo"
 	"github.com/merliot/dean/demo/connect"
 	"github.com/merliot/dean/demo/pyportal"
+	"github.com/merliot/dean/demo/wio"
 )
 
 func main() {
@@ -16,6 +17,7 @@ func main() {
 
 	demo.Register("demo-pyportal", pyportal.New)
 	demo.Register("demo-connect", connect.New)
+	demo.Register("demo-wio", wio.New)
 
 	log.Fatal(server.ServeTLS("demo.merliot.net"))
 }
