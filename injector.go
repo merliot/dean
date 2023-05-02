2f3b6a3f9ddf48aa496de2a292432965806f77db
Scott Feldman <sfeldma@gmail.com>
2023-05-02 03:46:57 +0000

    hide Injector internals

--- injector.go
+++ injector.go
@@ -6,11 +6,11 @@ type Injector struct {
 
 func NewInjector(name string, bus *Bus) *Injector {
 	i := &Injector{socket{name, "", 0, bus}}
-	bus.plugin(i)
+	bus.plugin(&i.sock)
 	return i
 }
 
 func (i *Injector) Inject(msg *Msg) {
-	msg.bus, msg.src = i.bus, i
-	i.bus.receive(msg)
+	msg.bus, msg.src = i.sock.bus, &i.sock
+	i.sock.bus.receive(msg)
 }
