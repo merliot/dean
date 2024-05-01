94d6f8dbf20dfcb7fc89b3c67f999cd095ea2aad
Scott Feldman <sfeldma@gmail.com>
2024-04-30 17:25:55 -0700

    Revert "wip: tagged msgs"
    This reverts commit e928c73e10db03d3c6acdaa6089aaae7907aba90.

--- socket.go
+++ socket.go
@@ -1,7 +1,5 @@
 package dean
 
-import "sync"
-
 // Socketer defines a socket interface
 type Socketer interface {
 	// Close the socket
@@ -10,12 +8,10 @@ type Socketer interface {
 	Send(*Msg) error
 	// Name of socket
 	String() string
-	// HasTag tests if socket has tag
-	HasTag(string) bool
-	// AddTag adds a tag to the socket.  A socket tag is like a VLAN ID.
-	AddTag(string)
-	// DelTag removes a tag from the socket.
-	DelTag(string)
+	// Tag returns the socket tag
+	Tag() string
+	// SetTag set the socket tag.  A socket tag is like a VLAN ID.
+	SetTag(string)
 	// SetFlag on socket
 	SetFlag(uint32)
 	// TestFlag returns true if flag is set
@@ -25,8 +21,7 @@ type Socketer interface {
 // socket implements Socketer
 type socket struct {
 	name  string
-	mu    sync.RWMutex
-	tag   map[string]bool
+	tag   string
 	flags uint32
 	bus   *Bus
 }
@@ -36,10 +31,6 @@ const (
 	// broadcast on this socket.
 	SocketFlagBcast uint32 = 1 << iota
 )
-
-func newSocket() *socket {
-	return &socket{tag: make(map[string]bool)}
-}
 
 func (s *socket) Close() {
 }
@@ -52,32 +43,18 @@ func (s *socket) String() string {
 	return s.name
 }
 
-func (s *socket) HasTag(string) bool {
-	s.mu.RWLock()
-	defer s.mu.RWUnlock()
+func (s *socket) Tag() string {
 	return s.tag
 }
 
-func (s *socket) AddTag(tag string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.tag[tag] = true
-}
-
-func (s *socket) DelTag(tag string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	delete(s.tag, tag)
+func (s *socket) SetTag(tag string) {
+	s.tag = tag
 }
 
 func (s *socket) SetFlag(flag uint32) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
 	s.flags |= flag
 }
 
 func (s *socket) TestFlag(flag uint32) bool {
-	s.mu.RWLock()
-	defer s.mu.RWUnlock()
 	return (s.flags & flag) != 0
 }
